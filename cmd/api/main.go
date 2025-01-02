8289cce90549156c73f4bff366ce0f510426e39d
mancuoj <mancuoj@gmail.com>
2025-01-02 20:08:32 +0800

    chore: update

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, Go React Tmpl!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Println("Starting server on http://localhost:4444")
+	err := http.ListenAndServe(":4444", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
 }
